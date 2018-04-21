b484e0bb48812fc1fd491ef07cd27d44e0d15c92
Sagar Naik <Sagar20688@gmail.com>
2018-04-21 21:59:42 +0530

    1st Commit in Core Java Project

---
+++ src/org/CoreJava/Program/EmployeeClass.java
@@ -0,0 +1,11 @@
+package org.CoreJava.Program;
+
+public class EmployeeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome employee :-");
+
+	}
+
+}
